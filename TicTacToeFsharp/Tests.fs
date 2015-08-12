4e70cdd13517b44b3cdb907510191604a6b4162a
Erlend Røsjø <erlend@erlend.net>
2015-08-12 12:22:16 +0200

    Refactored tests a bit

--- TicTacToeFsharp/Tests.fs
+++ TicTacToeFsharp/Tests.fs
@@ -1,49 +1,63 @@
 ﻿namespace TicTacToe.Tests
 
-module Tests =
+module Tests = 
     open FsUnit
     open NUnit.Framework
     
-    let [<Test>] ``when creating gameboard, board width should equal 3`` () =
-        Game.CreateGameboard 3 |> Array2D.length1 |> should equal 3
-
-    let [<Test>] ``when creating gameboard, board height should equal 3`` () =
-        Game.CreateGameboard 3 |> Array2D.length2 |> should equal 3
-
-    let [<Test>] ``count element should return correct count`` () =
-        [|"X"; "X"; "0";|] |> Game.CountElement "X" |> should equal 2
-
-    let [<Test>] ``get row should return correct row`` () =
-        let testArray = Array2D.create 3 3 ""
-        testArray.[0,0] <- "X";
-        testArray.[0,1] <- "X";
-        testArray.[0,2] <- "X";
-        Game.GetRow 0 testArray |> should equal [|"X"; "X"; "X";|]
-
-    let [<Test>] ``get column should return correct column`` () =
-        let testArray = Array2D.create 3 3 ""
-        testArray.[0,0] <- "X";
-        testArray.[1,0] <- "X";
-        testArray.[2,0] <- "X";
-        Game.GetColumn 0 testArray |> should equal [|"X"; "X"; "X";|]
-
-    let [<Test>] ``has filled row should return correct result`` () =
+    [<Test>]
+    let ``given a board width of 3, when creating gameboard, board width should be 3``() = 
+        Game.CreateGameboard 3
+        |> Array2D.length1
+        |> should equal 3
+    
+    [<Test>]
+    let ``given a board width of 3, when creating gameboard, board height should be 3``() = 
+        Game.CreateGameboard 3
+        |> Array2D.length2
+        |> should equal 3
+    
+    [<Test>]
+    let ``given a row having two "X" elements, when counting number of "X" elements, count should be 2``() = 
+        [| "X"; "X"; "0" |]
+        |> Game.CountElement "X"
+        |> should equal 2
+    
+    [<Test>]
+    let ``given a gameboard, when getting a row, result should be correct row``() = 
         let boardsize = 3
-        let marker = "X";
+        let element = "X"
+        let gameboard = Game.CreateGameboard boardsize
+        gameboard.[0, 0] <- element
+        gameboard.[0, 1] <- element
+        gameboard.[0, 2] <- element
+        Game.GetRow 0 gameboard |> should equal [| element; element; element |]
+    
+    [<Test>]
+    let ``given a gameboard, when getting a column, result should be correct column``() = 
+        let boardsize = 3
+        let element = "X"
+        let gameboard = Game.CreateGameboard boardsize
+        gameboard.[0, 0] <- element
+        gameboard.[1, 0] <- element
+        gameboard.[2, 0] <- element
+        Game.GetColumn 0 gameboard |> should equal [| element; element; element |]
+    
+    [<Test>]
+    let ``given a gameboard with first row filled, when calling HasFilledRow, shoud be true``() = 
+        let boardsize = 3
+        let marker = "X"
         let gameboard = Game.CreateGameboard boardsize
         gameboard.[0, 0] <- marker
         gameboard.[0, 1] <- marker
         gameboard.[0, 2] <- marker
-
         Game.HasFilledRow marker gameboard boardsize |> should equal true
-
-    let [<Test>] ``has filled column should return correct result`` () =
+    
+    [<Test>]
+    let ``given a gameboard with first column filled, when calling HasFilledColumn, shoud be true``() = 
         let boardsize = 3
-        let marker = "X";
+        let marker = "X"
         let gameboard = Game.CreateGameboard boardsize
         gameboard.[0, 0] <- marker
         gameboard.[1, 0] <- marker
         gameboard.[2, 0] <- marker
-
         Game.HasFilledColumn marker gameboard boardsize |> should equal true
-        
